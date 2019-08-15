c060a7cfb202b4f33e39742b8d040ec460df8af7
IanB13 <48295744+IanB13@users.noreply.github.com>
2019-08-14 18:18:49 -0600

    Adding unicafe from desktop

--- Part1/unicafe/src/index.js
+++ Part1/unicafe/src/index.js
@@ -1,35 +1,26 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
-
-  return (
-    <div>
+const Hello = () => {
+    return (
       <div>
-        {left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
-        {right}
-        <p>{allClicks.join(' ')}</p>
+        <p>Hello world</p>
+        <p>This is nice</p>
+        <input type="button"></input>
       </div>
-    </div>
-  )
+    )
+  }
+
+const App = () => {
+    return (
+        <div>
+          <h1>Greetings</h1>
+          <Hello />
+        </div>
+      )
 }
 
 ReactDOM.render(
-  <App  />, 
+  React.createElement(App, null),
   document.getElementById('root')
 )
