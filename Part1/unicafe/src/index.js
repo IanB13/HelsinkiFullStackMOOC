eba4c14bedbdc4ebc2917944473f999583e3674e
IanB13 <48295744+IanB13@users.noreply.github.com>
2019-08-16 14:30:40 -0600

    testing commit from VS code

--- Part1/unicafe/src/index.js
+++ Part1/unicafe/src/index.js
@@ -1,26 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = () => {
-    return (
-      <div>
-        <p>Hello world</p>
-        <p>This is nice</p>
-        <input type="button"></input>
-      </div>
-    )
-  }
 
-const App = () => {
-    return (
-        <div>
-          <h1>Greetings</h1>
-          <Hello />
-        </div>
-      )
+const Statistcs = (props) =>{
+  const [good,neutral,bad] = props.feedback;
+  let all = 0;
+
+  props.feedback.forEach(input => {
+   all = all+input
+  });
+  
+  if(all === 0){
+    return(
+      <>
+      <h1>statistcs </h1>
+      <div>
+        No feedback given
+      </div>
+      </>
+    )
+
+  }
+  return (
+    <>
+      <h1>statistcs </h1>
+      <table>
+        <tbody>
+          <tr>
+            <Statistc text="good" value={good} />
+          </tr>
+          <tr>
+            <Statistc text="neutral" value={neutral} />
+          </tr>
+          <tr>
+            <Statistc text="bad" value={bad} />
+          </tr>
+          <tr>
+            <Statistc text="all" value={all} />
+          </tr>
+          <tr>
+            <Statistc text="average" value={Math.round(100 * ((good - bad) / all))} percent="true" />
+          </tr>
+          <tr>
+            <Statistc text="positive" value={Math.round(100 * (good / all))} percent="true" />
+          </tr>
+        </tbody>
+      </table>
+    </>
+  )
+
 }
 
-ReactDOM.render(
-  React.createElement(App, null),
+const Statistc = (props) => {
+let value = props.value
+
+// using string comparison for conditional, should look into changing
+if(props.percent === "true"){
+  value = +value +"%"
+}
+  return(
+    <>
+      <td>
+        {props.text}
+      </td>
+      <td>
+        {value}
+      </td>
+    </>
+  )
+
+}
+
+
+
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <>
+      <div id = "feedbackbuttons">
+        <h1>give feedback</h1>
+        <button onClick ={() => setGood(good+1)}>
+          good
+        </button>
+        <button onClick ={() => setNeutral(neutral+1)}>
+          neutral
+        </button>      
+        <button onClick ={() => setBad(bad+1)}>
+        bad
+        </button>
+      </div>
+      <Statistcs  feedback = {[good,neutral,bad]} />
+    </>
+    
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
 )
