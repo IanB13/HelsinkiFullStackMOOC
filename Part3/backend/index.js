c04b535aba84b8d5d4a17e686ef1215ce3a8bc1c
IanB13 <48295744+IanB13@users.noreply.github.com>
2019-11-06 15:41:06 -0700

    Added Express Nodemon

--- Part3/backend/index.js
+++ Part3/backend/index.js
@@ -1,12 +1,64 @@
-const http = require('http')
+const express = require('express');
+const app =express();
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World')
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
+  },
+  {
+    id: 4,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
+  }
+]
+
+
+app.get('/', (req,res) =>{
+  res.send('<h1> Landing</h1>')
+} )
+
+app.get('/notes', (req, res) => {
+  res.json(notes)
+})
+
+app.get('/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find(note => note.id === id)
+  if(note){
+    response.status(200).json(note);
+  }
+  else{
+    response.status(404).end();
+  }
+ 
+
+})
+
+app.delete('/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
 })
 
 
-
-const port = 3002
-app.listen(port)
-console.log(`Server running on port ${port}`)
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
