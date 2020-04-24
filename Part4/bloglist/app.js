0908fb1f50e8eefb0f2575f940ae359f977d2578
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-04-24 16:59:21 -0600

    refactored to add app

---
+++ Part4/bloglist/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger') //logger for adding logging frameworks in the future
+const config = require('./utils/config') //handles .env configuration
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true,useUnifiedTopology: true  }).then(success =>{
+  logger.info(`connected at ${config.MONGODB_URI}`)
+}
+).catch( err =>{
+  logger.error(err)
+})
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
--- Part4/bloglist/index.js
+++ Part4/bloglist/index.js
@@ -1,23 +1,10 @@
-const config = require('./utils/config') //handles .env configuration
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const blogRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
-const logger = require('./utils/logger') //logger for adding logging frameworks in the future
+const app = require('./app') // the actual Express app
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true,useUnifiedTopology: true  }).then(success =>{
-  logger.info(`connected at ${config.MONGODB_URI}`)
-}
-).catch( err =>{
-  logger.error(err)
-})
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use('/api/blogs', blogRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
