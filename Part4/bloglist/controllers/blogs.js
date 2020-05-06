ed5ed661929c61deeba0c522ae66db7648c7f8c9
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-05-06 13:19:51 -0600

    4.19 posts now require bearer token auth

--- Part4/bloglist/controllers/blogs.js
+++ Part4/bloglist/controllers/blogs.js
@@ -2,6 +2,15 @@ const blogRouter = require('express').Router()
 const Blog = require(`../models/Blog`) //gets moongoose model
 const User = require('../models/User')
 const logger = require('../utils/logger')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogRouter.get('/', async (request, response) => {
     const blogs = await  Blog.find({})
@@ -11,7 +20,15 @@ blogRouter.get('/', async (request, response) => {
   })
 
 blogRouter.post('/', async (request, response) => {
+
     const body = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+   
+    if (!token || !decodedToken.id) {
+      console.log("here")
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
     if( !(body.title || body.url)){
       response.status(400).json({ 
         error: 'content missing'
@@ -19,7 +36,7 @@ blogRouter.post('/', async (request, response) => {
       logger.info('bad request')
     }
     else{
-    const userData = await User.findById(body.userId)
+    const userData = await User.findById(decodedToken.id)
     const user = {
       username:userData.username,
       name: userData.name,
