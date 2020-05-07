b77cb1bea16a48148a6c19fefe3aeab381f00902
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-05-06 21:46:58 -0600

    4.20 refactored token auth to middleware

--- Part4/bloglist/app.js
+++ Part4/bloglist/app.js
@@ -22,6 +22,7 @@ app.use(middleware.requestLogger)
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
