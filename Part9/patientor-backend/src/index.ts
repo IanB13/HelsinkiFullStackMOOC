0b9c16b732f8c34b61aea4ad14dd60e2300cbae5
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-06-22 11:39:45 -0600

    9.10 still needs types

--- Part9/patientor-backend/src/index.ts
+++ Part9/patientor-backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import diagnosesRouter from './routes/diagnoses';
 const app = express();
 import cors from 'cors';
 app.use(express.json());
@@ -11,7 +12,9 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
-  
+
+app.use("/api/diagnoses",diagnosesRouter); 
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ Part9/patientor-backend/src/services/diagnosesService.ts
@@ -0,0 +1,14 @@
+import diagnosesData from '../data/diagnoses.json';
+
+const getEntries = () => {
+    return diagnosesData;
+  };
+  
+  const addEntry = ():null => {
+    return null;
+  };
+  
+  export default {
+    getEntries,
+    addEntry
+  };
