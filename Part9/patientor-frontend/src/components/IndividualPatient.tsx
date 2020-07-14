d42a5bd23e97f106a137366f80d73b4f150f2282
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-07-14 16:57:09 -0600

    Added front-end

---
+++ Part9/patientor-frontend/src/components/IndividualPatient.tsx
@@ -0,0 +1,41 @@
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
+import {
+    useParams
+  } from "react-router-dom";
+import {apiBaseUrl} from '../constants';
+
+
+const IndividualPatient = () =>{
+    const [data,changeData] = useState({loading:true, id: null,
+        name: null,
+        occupation: null,
+        gender: null,
+        ssn: null,
+        dateOfBirth: null});
+
+    const { id } = useParams<{ id: string }>();
+    useEffect(()=>{
+        axios.get(`${apiBaseUrl}/patients/${id}`).then(
+            response => changeData(response.data)
+        );
+    },[]);
+
+
+    if(data.loading){
+    return(<div>
+       loading...
+        </div>);
+    }
+    else{
+        return(<div>
+            {data.name} <br/>
+            BOB: {data.dateOfBirth} <br/>
+            occupation: {data.occupation}
+            
+        </div>);
+    }
+
+};
+
+export default IndividualPatient;
