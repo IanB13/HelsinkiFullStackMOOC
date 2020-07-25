e365dce4c86f0c9be1eb53ba921a0db92dc89ee8
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-07-25 13:36:20 -0600

    Added diagnostic names

--- Part9/patientor-frontend/src/components/Entries/Diagnoses.tsx
+++ Part9/patientor-frontend/src/components/Entries/Diagnoses.tsx
@@ -1,12 +1,28 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect,useState} from 'react';
+import Axios from 'axios'; //should put this as a state and attach it to the Redux Store
+import {Diagnoses as DiagnosesType} from '../../types';
 
 const Diagnoses: FC<{diagnoses: string[]|undefined }> = ({diagnoses}) =>{
-console.log(diagnoses);
+const [diagnosesList, setDiagnosesList] = useState< null | DiagnosesType[] >(null);
+    //ineffective use, will re-render everytime
+    //Should make Redux action
+    useEffect( ()=>{
+        Axios.get("http://localhost:3001/api/diagnoses").then(
+        (Response) =>{
+            console.log(Response.data);
+             setDiagnosesList(Response.data);
 
-if(diagnoses){
+        }
+        );
+    },[]);
+
+if(diagnoses && diagnosesList){
 return(
     <>
-    {diagnoses.map(die => <div key ={die}> {die} </div>)}
+    {diagnoses.map(diagnose => <div key ={diagnose}>
+         {diagnose}:
+         {diagnosesList.find((list)=>list.code === diagnose)?.name}
+         </div>)}
     </>
 );
 }
